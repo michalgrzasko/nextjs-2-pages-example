3a0c26065a0564e827b600c3af04d22eca745f0a
Michał Grząśko <m.grzasko@uruzlabs.com>
2022-05-12 18:04:17 +0200

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Nav } from '../src/Nav/Nav';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
+
+  const landingUrl = process.env.NEXT_PUBLIC_LANDING_PAGE_BASE_URL as string;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,44 +18,12 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Dashboard
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Link href="/settings">Settings</Link>
+        <Link href={landingUrl}>Sign out</Link>
       </main>
 
       <footer className={styles.footer}>
@@ -61,12 +34,20 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            {/*<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />*/}
           </span>
         </a>
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+
+    }
+  }
 }
 
 export default Home
